eff5902ded7cacefab8da533c368da1d8d4900dc
kimchankwon <kimchankwon@gmail.com>
2025-08-19 17:54:34 +1000

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,28 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `exceed`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "R-8Pc1DEyUhrFa5Bz3JWmtBixeoEYAVahWokt2PeTm0",
+      "spaceId": "9exkktpmehws"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
+
+export default config;
