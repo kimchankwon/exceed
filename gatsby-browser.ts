4a1e9753790c671f9c2cd5def7a32eec36c98faa
daniel-kim-sonder <daniel.kim@sonder.io>
2026-03-24 10:56:59 +1100

    feat: add fontsource packages and update global styles for typography

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,3 +1,7 @@
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/800.css";
+import "@fontsource/work-sans/400.css";
+import "@fontsource/work-sans/500.css";
 import "./src/styles/global.css";
 import * as React from "react";
 import type { GatsbyBrowser } from "gatsby";
--- gatsby-ssr.ts
+++ gatsby-ssr.ts
@@ -1,3 +1,7 @@
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/800.css";
+import "@fontsource/work-sans/400.css";
+import "@fontsource/work-sans/500.css";
 import * as React from "react";
 import type { GatsbySSR } from "gatsby";
 import Layout from "./src/components/Layout";
